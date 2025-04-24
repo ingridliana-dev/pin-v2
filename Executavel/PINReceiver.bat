7e35d315b24dceddcd98ca0ec3673ef5bdddc4f4
ingridliana-dev <ingridliana.dev@gmail.com>
2025-04-24 16:39:26 -0300

    Melhorado script de inicialização para mostrar console com mensagens de log

--- Executavel/PINReceiver.bat
+++ Executavel/PINReceiver.bat
@@ -1,2 +1,9 @@
 @echo off
-powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0\..\PINReceiver.ps1"
+echo Iniciando Receptor de PIN...
+echo.
+echo Este console mostrará mensagens de log e erros.
+echo Não feche esta janela enquanto o aplicativo estiver em execução.
+echo.
+echo Pressione Ctrl+C para encerrar o aplicativo.
+echo.
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0\..\PINReceiver.ps1"
